f466d30d5c525ab00471236be015e9dc3d646cb6
christelva <cvanaanholt@hotmail.com>
2017-12-22 07:48:00 +0100

    Most tests pass

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -1,7 +1,10 @@
 class Codebreaker
   class Game
     attr_reader :output
-WRONG_FORMATTED_GUESS = "Try guessing a number with 4 digits"
+    WRONG_FORMATTED_GUESS = "Try guessing a number with four digits"
+    NUMBER_MATCH = "-"
+    EXACT_MATCH = "+"
+    NO_MATCH = ''
 
 
     def initialize(output)
@@ -15,23 +18,35 @@ WRONG_FORMATTED_GUESS = "Try guessing a number with 4 digits"
       output.puts "Enter guess:"
     end
 
-    def guess(input)
-        unless input.length == 4
-          puts WRONG_FORMATTED_GUESS
-
+    def correct_format(input)
+        if input.length != 4
+          output.puts WRONG_FORMATTED_GUESS
+          return false
+        else
+          return true
         end
+      end
 
-        if input.chars.map.with_index do |e, i|
+      def guess(input)
+          if correct_format(input) == true
+          else
+            return
+         end
 
-          case @secret_number.chars.index(e)
-            when nil then print''
-            when i then print '+'
-            else  print '-'
+          secret_numbers = @secret_number.chars
+          input_array = input.chars
+
+          result = []
+          input_array.map.with_index do |number, index|
+          if input_array[index] == secret_numbers[index]
+            result << EXACT_MATCH
+          elsif secret_numbers.include?(number)
+            result << NUMBER_MATCH
+          else
+             result << NO_MATCH
           end
-        end.join
-
-  output.puts "\n You typed: #{input}"
+        end
+        output.puts result.sort.join("")
+        end
+      end
     end
-  end
-end
-end
